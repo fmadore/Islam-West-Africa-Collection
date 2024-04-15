232aaae2dfdc4937cb58454b2736cc659b70b79b
fmadore <frederick_madore@outlook.com>
2024-04-15 10:43:41 +0200

    Multilingual pie chart

--- Visualisations/items_languages.py
+++ Visualisations/items_languages.py
@@ -4,38 +4,39 @@ from collections import defaultdict
 from tqdm import tqdm
 
 api_url = "https://iwac.frederickmadore.com/api"
-resource_classes = [35, 43, 88, 40, 82, 178, 52, 77, 305, 58, 49, 36, 60, 38]  # Extended list of class IDs
+resource_classes = [35, 43, 88, 40, 82, 178, 52, 77, 305, 58, 49, 36, 60, 38]  # Resource class IDs
 
 def fetch_items_by_class(resource_classes):
-    """ Fetch all items that belong to specific resource classes, handling pagination with a progress bar. """
+    """ Fetch all items for specified classes, extracting language info from dcterms:language. """
     language_count = defaultdict(int)
     page = 1
-    total_fetched = 0
+    total_items = 0  # You can use this to show a rough estimate in the progress bar, if API supports total count
     with tqdm(desc="Fetching items", unit=" items") as pbar:
         while True:
             response = requests.get(f"{api_url}/items", params={"resource_class_id[]": resource_classes, "page": page, "per_page": 50})
             data = response.json()
             if not data:
-                break  # Stop if no more data is returned
-            num_items = len(data)
-            total_fetched += num_items
-            pbar.update(num_items)
+                break
             for item in data:
                 languages = item.get('dcterms:language', [])
                 for lang in languages:
-                    if 'display_title' in lang:
-                        language_count[lang['display_title']] += 1
+                    language_label = lang['display_title']  # Assuming this contains the proper language label
+                    language_count[language_label] += 1
             page += 1
+            pbar.update(len(data))
     return language_count
 
-def create_pie_chart(language_count):
+def create_pie_chart(language_count, title='Distribution of items by language'):
     """ Create a pie chart from the language distribution data. """
     labels = list(language_count.keys())
     values = list(language_count.values())
 
-    fig = px.pie(names=labels, values=values, title='Distribution of Items by Language')
+    fig = px.pie(names=labels, values=values, title=title)
     fig.show()
 
-# Execute the functions
+# Fetch and visualize data
 language_distribution = fetch_items_by_class(resource_classes)
-create_pie_chart(language_distribution)
+create_pie_chart(language_distribution, title='Distribution of items by language')  # English version
+
+# Assuming French translation for the chart title is needed
+create_pie_chart(language_distribution, title='Répartition des éléments par langue')  # French version
