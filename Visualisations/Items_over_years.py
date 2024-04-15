3533cf6e4bde1271d9e27757293e2d9c2049c978
fmadore <frederick_madore@outlook.com>
2024-04-15 08:32:21 +0200

    multilingual support

--- Visualisations/Items_over_years.py
+++ Visualisations/Items_over_years.py
@@ -14,6 +14,23 @@ acceptable_ids = {
     38: "AudioVisualDocument"
 }
 
+# Define labels for both English and French
+labels = {
+    'en': {
+        'title': 'Number of Items by Type Over Years',
+        'number_of_items': 'Number of Items',
+        'year': 'Year',
+        'type': 'Item Type',
+        'filename': 'item_distribution_over_years_english.html'
+    },
+    'fr': {
+        'title': 'Nombre d\'éléments par type au fil des ans',
+        'number_of_items': 'Nombre d\'éléments',
+        'year': 'Année',
+        'type': 'Type d\'élément',
+        'filename': 'item_distribution_over_years_french.html'
+    }
+}
 
 def fetch_items():
     page = 1
@@ -29,22 +46,25 @@ def fetch_items():
             else:
                 pbar.update(1)
                 for item in data:
-                    resource_classes = item.get('o:resource_class')
+                    resource_classes = item.get('o:resource_class', {})
                     if resource_classes:
-                        # Extract class IDs
+                        item_classes = []
+                        # Check if resource_classes is a list of dictionaries
                         if isinstance(resource_classes, list):
-                            item_classes = [rclass.get("o:id") for rclass in resource_classes if
-                                            rclass.get("o:id") in acceptable_ids]
+                            item_classes = [rclass.get("o:id") for rclass in resource_classes if rclass.get("o:id") in acceptable_ids]
+                        # Check if resource_classes is a dictionary
                         elif isinstance(resource_classes, dict):
-                            item_classes = [resource_classes.get("o:id")] if resource_classes.get(
-                                "o:id") in acceptable_ids else []
+                            resource_class_id = resource_classes.get("o:id")
+                            if resource_class_id in acceptable_ids:
+                                item_classes.append(resource_class_id)
                         else:
+                            # If resource_classes is neither a list nor a dictionary, skip processing it
                             continue
 
                         # Extract year from date
                         date_info = item.get('dcterms:date', [])
                         if date_info:
-                            date_value = date_info[0]['@value']
+                            date_value = date_info[0].get('@value', '')
                             year = date_value.split('-')[0]
                             for id in item_classes:
                                 items_by_year_type[year][acceptable_ids[id]] += 1
@@ -52,26 +72,29 @@ def fetch_items():
 
     return items_by_year_type
 
-
-def visualize_items_over_years(items_by_year_type):
+def visualize_items_over_years(items_by_year_type, language='en'):
     # Prepare data for visualization
     data = []
     for year, types in sorted(items_by_year_type.items()):  # Ensure data is sorted by year
         for type_name, count in types.items():
             data.append({'Year': year, 'Type': type_name, 'Number of Items': count})
 
+    # Use language-specific labels
+    label = labels[language]
+
     # Create a stacked bar chart
     fig = px.bar(data, x='Year', y='Number of Items', color='Type',
-                 title='Number of Items by Type Over Years',
-                 labels={'Number of Items': 'Number of Items', 'Year': 'Year', 'Type': 'Item Type'},
+                 title=label['title'],
+                 labels={'Number of Items': label['number_of_items'], 'Year': label['year'], 'Type': label['type']},
                  hover_data={'Number of Items': True})
     fig.update_traces(textposition='inside')
     fig.update_layout(barmode='stack', xaxis={'type': 'category', 'categoryorder': 'category ascending'})
-    fig.update_traces(texttemplate='', hovertemplate='<b>%{x}</b><br>%{y} items<br>%{text}')
-    fig.write_html('item_distribution_over_years.html')
+    fig.write_html(label['filename'])
     fig.show()
 
-
-# Fetch data and create visualization
+# Fetch data
 items_by_year_type = fetch_items()
-visualize_items_over_years(items_by_year_type)
+
+# Create visualization in English and French
+visualize_items_over_years(items_by_year_type, language='en')
+visualize_items_over_years(items_by_year_type, language='fr')
